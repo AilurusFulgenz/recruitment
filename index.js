50b70b35cd1c7ce75f2c09bb0a80bae65b0ca4d0
Stormshield <stormshield@demo1.arkoon.dev>
2018-11-12 11:49:31 +0100

    improvments

--- index.js
+++ index.js
@@ -9,6 +9,9 @@ const filteringRules = [
     new FilteringRule({src: '192.168.0.0', dst: '10.0.0.0', port: 25}),
     new FilteringRule({src: '192.168.1.0', dst: '10.0.1.0', port: 22}),
 ];
+function getFilteringRulesAsJSON() {
+    return filteringRules;
+}
 
 const app = express();
 
@@ -16,12 +19,16 @@ app.use(bodyParser.json());
 app.use(express.static('frontApp'));
 
 app.get('/filtering-rules/', (req, res) => {
-    res.send(filteringRules);
+    res.send(getFilteringRulesAsJSON());
+});
+
+app.get('/filtering-rules/:filteringRuleID', (req, res) => {
+    res.send(`You asked for filtering rule #${req.params.filteringRuleID}`);
 });
 
 app.post('/filtering-rules/', (req, res) => {
     filteringRules.push(new FilteringRule(req.body));
-    res.send(filteringRules);
+    res.send(getFilteringRulesAsJSON());
 });
 
 module.exports = app;
